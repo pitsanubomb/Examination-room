efcd91524218537e5a938f9d1e7b798313de0306
pitsanubomb <pitsanubomb@gmail.com>
2020-10-19 17:35:35 +0700

    :fire: Fix UI ant and add nav but not yet :rocket:

---
+++ src/components/Layout/BackofficeUI.vue
@@ -0,0 +1,65 @@
+<template>
+  <a-layout id="components-layout-demo-top-side-2">
+    <a-layout-header class="header">
+      <div class="logo" />
+    </a-layout-header>
+    <a-layout>
+      <a-layout-sider width="200" style="background: #fff">
+        <a-menu
+          mode="inline"
+          v-model:selectedKeys="selectedKeys2"
+          v-model:openKeys="openKeys"
+          :style="{ height: '100%', borderRight: 0 }"
+        >
+          <a-sub-menu key="sub1">
+            <template v-slot:title>
+              <span><user-outlined />subnav 1</span>
+            </template>
+            <a-menu-item key="1">Manage room</a-menu-item>
+            <a-menu-item key="2">option2</a-menu-item>
+            <a-menu-item key="3">option3</a-menu-item>
+            <a-menu-item key="4">option4</a-menu-item>
+          </a-sub-menu>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout style="padding: 0 24px 24px">
+        <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>Exam</a-breadcrumb-item>
+          <a-breadcrumb-item>Room</a-breadcrumb-item>
+          <a-breadcrumb-item>manage</a-breadcrumb-item>
+        </a-breadcrumb>
+        <a-layout-content
+          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '580px' }"
+        >
+          <router-view />
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </a-layout>
+</template>
+<script>
+import { UserOutlined } from '@ant-design/icons-vue';
+
+export default {
+  components: {
+    UserOutlined,
+  },
+  data() {
+    return {
+      selectedKeys2: ['1'],
+      collapsed: false,
+      openKeys: ['sub1'],
+    };
+  },
+};
+</script>
+
+<style>
+#components-layout-demo-top-side-2 .logo {
+  width: 120px;
+  height: 31px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px 28px 16px 0;
+  float: left;
+}
+</style>
